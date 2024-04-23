79b83f92caeda9b16a4305bc401e99ed9fece0a7
Adrian Perez <adrianp1299@gmail.com>
2024-04-23 17:04:54 -0600

    15~18

--- index.js
+++ index.js
@@ -119,6 +119,8 @@ class Car {
     this.odometer = 0 // car initilizes with zero miles
     this.tank = tankSize // car initiazes full of gas
     // ✨ initialize whatever other properties are needed
+    this.tankSize = tankSize 
+    this.mpg = mpg
   }
 
   /**
@@ -136,6 +138,15 @@ class Car {
    */
   drive(distance) {
     // ✨ implement
+    const milesCanDrive = this.tank * this.mpg
+     if(distance <= milesCanDrive) {
+     this.odometer = this.odometer + distance
+     this.tank =  this.tank - (distance/this.mpg)
+    } else {
+    this.odometer = this.odometer + milesCanDrive
+    this.tank = 0
+    }
+     return this.odometer
   }
 
   /**
@@ -151,6 +162,13 @@ class Car {
    */
   refuel(gallons) {
     // ✨ implement
+    const gallonsThatFit = this.tankSize - this.tank
+    if (gallons <= gallonsThatFit) {
+      this.tank = this.tank + gallons
+    } else {
+      this.tank = this.tankSize
+    }
+    return this.tank * this.mpg
   }
 }
 
